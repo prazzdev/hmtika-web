657c90189b7a5b3787a39c52b49b57842bb91379
prazzdev <prazzid31@gmail.com>
2023-08-31 15:23:44 +0700

    new file:   public/src/image/team/default.webp 	modified:   src/components/partials/ProfileBox/index.jsx

--- src/components/partials/ProfileBox/index.jsx
+++ src/components/partials/ProfileBox/index.jsx
@@ -1,4 +1,4 @@
-export default function ProfileBox({ thumb, name, position, email, linkedIn, github }) {
+export default function ProfileBox({ thumb='default', name, position, email, linkedIn, github }) {
     return (
         <div className="flex flex-col justify-center w-full px-8 mx-6 my-12 text-center rounded-md md:w-96 lg:w-80 xl:w-64 dark:bg-gray-100 dark:text-gray-800">
             <img alt="" className="self-center flex-shrink-0 w-24 h-24 -mt-12 bg-center bg-cover rounded-full dark:bg-gray-500" src={'/src/image/team/'+thumb+'.webp'} />
