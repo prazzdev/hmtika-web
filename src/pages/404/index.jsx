5c4e5e7144cc32098d11604028246be83b3a0f48
prazzdev <prazzid31@gmail.com>
2023-08-31 17:15:02 +0700

    modified:   src/pages/404/index.jsx

--- src/pages/404/index.jsx
+++ src/pages/404/index.jsx
@@ -8,7 +8,7 @@ export default function notFound() {
                 <div className="w-full text-center px-4">
                     <Image src={'/src/image/404.svg'} width={200} height={300} className="mx-auto mb-6" />
                     <p className="text-2xl font-semibold md:text-3xl">Maaf, halaman yang kamu tuju ngga ada nih.</p>
-                    <p className="mt-4 mb-8 text-gray-400">Jangan bingung, kamu bisa pilih menu lain di beranda ya!</p>
+                    <p className="mt-4 mb-8 text-gray-400">It's okay, kamu bisa pilih menu lain di beranda ya!</p>
                     <a rel="noopener noreferrer" href="../" className="w-full px-8 py-3 font-semibold rounded bg-pteal text-gray-100">Kembali ke Beranda</a>
                 </div>
             </div>
