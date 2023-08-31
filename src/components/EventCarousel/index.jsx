ba735fe3c64336fa538f04e15ca85460f0dacba6
prazzdev <prazzid31@gmail.com>
2023-08-31 22:31:21 +0700

    modified:   src/components/EventCarousel/index.jsx 	renamed:    src/pages/dokumentasi-kegiatan/index.jsx -> src/pages/dokumentasi-kegiatans/index.jsx

--- src/components/EventCarousel/index.jsx
+++ src/components/EventCarousel/index.jsx
@@ -1,37 +1,22 @@
-export default function EventCarousel({ title }) {
+export default function EventCarousel({ title, slideList }) {
     return (
         <>
         <div id="" className="">
             <h1 className="">{title}</h1>
             <div className="carousel w-full h-[300px]">
-                <div id="slide1" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide4" className="btn btn-circle">❮</a> 
-                    <a href="#slide2" className="btn btn-circle">❯</a>
-                    </div>
-                </div> 
-                <div id="slide2" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a> 
-                    <a href="#slide3" className="btn btn-circle">❯</a>
-                    </div>
-                </div> 
-                <div id="slide3" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide2" className="btn btn-circle">❮</a> 
-                    <a href="#slide4" className="btn btn-circle">❯</a>
-                    </div>
-                </div> 
-                <div id="slide4" className="carousel-item relative w-full">
-                    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide3" className="btn btn-circle">❮</a> 
-                    <a href="#slide1" className="btn btn-circle">❯</a>
-                    </div>
-                </div>
+                {slideList.map((slide) => {
+                    return (
+                        <>
+                        <div id={'#'+slide.id} className="carousel-item relative w-full">
+                            <img src={slide.imgSrc} className="w-full" />
+                            <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                            <a href={'#slide'+slide.leftBtn} className="btn btn-circle">❮</a> 
+                            <a href={'#slide'+slide.rightBtn} className="btn btn-circle">❯</a>
+                            </div>
+                        </div> 
+                        </>
+                    )
+                })}
             </div>
         </div>
         </>
