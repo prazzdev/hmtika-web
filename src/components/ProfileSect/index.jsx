83f70611319f21c97a4d812e9862ab6d9f4c8142
prazzdev <prazzid31@gmail.com>
2023-08-31 12:21:54 +0700

    modified:   src/components/ProfileSect/index.jsx 	new file:   src/pages/susunan-pengurus/index.jsx

--- src/components/ProfileSect/index.jsx
+++ src/components/ProfileSect/index.jsx
@@ -10,8 +10,18 @@ export default function ProfileSect() {
                 <h1 className="uppercase font-bold text-xl mx-auto mb-5">Profil</h1>
                 <div className="">
                     <p className="text-justify">
-                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non repudiandae veritatis, molestiae distinctio unde voluptates voluptatum aliquam exercitationem amet fugit voluptatibus velit laboriosam tenetur dolores nam, possimus accusamus, accusantium error?
+                        Himpunan Mahasiswa Informatika (HMTIKA) merupakan salah satu himpunan bagi mahasiswa jurusan di STIMIK Tunas Bangsa Banjarnegara prodi Informatika.
                     </p>
+                    <div className="flex justify-around mt-10">
+                        <div id='box' className="flex flex-col justify-center items-center">
+                            <h1 className='text-2xl mb-1'>13</h1>
+                            <h5>Pengurus</h5>
+                        </div>
+                        <div id='box' className="flex flex-col justify-center items-center">
+                        <h1 className='text-2xl mb-1'>6</h1>
+                            <h5>Bidang</h5>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
