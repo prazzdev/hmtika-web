e86bfe9695a0b61bb168c4d76ef0f6f8db9653c2
prazzdev <prazzid31@gmail.com>
2023-09-01 07:43:27 +0700

    modified:   src/pages/pdh-prodi/index.jsx

--- src/pages/pdh-prodi/index.jsx
+++ src/pages/pdh-prodi/index.jsx
@@ -7,10 +7,10 @@ export default function pdhProdi() {
         <Layout pageTitle={'PDH Prodi'} >
             <section 
                     className="pt-[100px] pb-[40px]"
-                    // data-aos="fade-in"
-                    // data-aos-duration="1000"
-                    // data-aos-delay="300"
-                    // data-aos-easing="ease-in-out"
+                    data-aos="fade-in"
+                    data-aos-duration="1000"
+                    data-aos-delay="300"
+                    data-aos-easing="ease-in-out"
                 >
                 <h1 className="text-xl text-pgray font-bold text-center mb-10">Pakaian Dinas Harian (PDH) Prodi</h1>
                 <Image src={'/src/image/pdh.webp'} height={300} width={300} className="mx-auto shadow-md" />
