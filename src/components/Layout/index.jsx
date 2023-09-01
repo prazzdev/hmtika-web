dfa717f01af91bb7522ce1e700b38c92e3575828
prazzdev <prazzid31@gmail.com>
2023-09-01 07:56:56 +0700

    deleted:    public/next.svg 	deleted:    public/vercel.svg 	modified:   src/components/Layout/index.jsx

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -18,17 +18,17 @@ export default function Layout({ children, pageTitle }) {
                 <meta name="description" content="Himpunan Mahasiswa Informatika (HMTIKA) merupakan salah satu himpunan bagi mahasiswa jurusan di STIMIK Tunas Bangsa Banjarnegara program studi Informatika." />
 
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://hmtika.vercel.app/" />
+                <meta property="og:url" content="https://hmtika.site" />
                 <meta property="og:title" content={title} />
                 <meta property="og:description" content="Himpunan Mahasiswa Informatika (HMTIKA) merupakan salah satu himpunan bagi mahasiswa jurusan di STIMIK Tunas Bangsa Banjarnegara program studi Informatika." />
-                <meta property="og:image" content="/src/image/logo.webp" />
+                <meta property="og:image" content="https://hmtika.site/src/image/logo.webp" />
 
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://hmtika.vercel.app/" />
+                <meta property="twitter:url" content="https://hmtika.site" />
                 <meta property="twitter:title" content={title} />
                 <meta property="twitter:description" content="Himpunan Mahasiswa Informatika (HMTIKA) merupakan salah satu himpunan bagi mahasiswa jurusan di STIMIK Tunas Bangsa Banjarnegara program studi Informatika." />
-                <meta property="twitter:image" content="/src/image/logo.webp" />
-                <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+                <meta property="twitter:image" content="https://hmtika.site/src/image/logo.webp" />
+                <link rel="icon" type="image/x-icon" href="https://hmtika.site/favicon.ico" />
                 <title>{title}</title>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </Head>
