d591386bf84deea87ce5f5f86893456fbb3bcd68
prazzdev <prazzid31@gmail.com>
2023-09-23 01:20:04 +0700

    modified:   public/sw.js.map 	modified:   src/pages/chatbot/index.jsx

--- src/pages/chatbot/index.jsx
+++ src/pages/chatbot/index.jsx
@@ -4,12 +4,13 @@ import { useState } from "react"
 let _url = 'https://generativelanguage.googleapis.com/v1beta2/models/text-bison-001:generateText?key=AIzaSyBvQ8j8TDPTTqnRU7pgQZeDeqfFSLjFniA'
 
 export default function index() {
-    const [keyword, setKeyword] = useState('')
+    const [keyword="", setKeyword] = useState('')
+    // console.log(keyword)
 
-    async function search() {
-        const query = await searchData()
-        setKeyword(query.candidates[0].output)
-        console.log({ query: query })
+    async function search(q) {
+        const { candidates } = await searchData(q)
+        const result = candidates[0].output
+        setKeyword(result)
     }
 
     return (
@@ -29,14 +30,14 @@ export default function index() {
     )
 }
 
-export async function searchData() {
+export async function searchData(q) {
     const res = await 
                 fetch(_url, {
                     method: 'POST',
                     headers: {
                     'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ "prompt": { "text": "g30s/pki itu apa?"} })
+                    body: JSON.stringify({ "prompt": { "text": q} })
                 })
     const data = await res.json()
     return data
