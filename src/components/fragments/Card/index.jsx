d4e769e6f60fa5fc8285e81bb0fbb74022464440
Agung Prasetyo <prazzid31@gmail.com>
2025-01-20 21:10:27 +0700

    Update Card/index.jsx

--- src/components/fragments/Card/index.jsx
+++ src/components/fragments/Card/index.jsx
@@ -16,8 +16,8 @@ export default function Card({
   }
   return (
     <Link
-      href={target}
-      className="relative bg-gray-200 shadow-md mb-8 p-6 rounded-lg lg:min-w-[20svw] lg:max-w-[25svw] lg:min-h-[54svh] 2xl:min-h-[40svh] text-gray-800"
+      href={"/blog/" + target}
+      className="relative bg-gray-200 shadow-md mb-8 p-6 rounded-lg lg:min-w-[20svw] lg:max-w-[20svw] lg:min-h-[45svh] 2xl:min-h-[40svh] text-gray-800"
     >
       <Image
         width={300}
@@ -30,11 +30,11 @@ export default function Card({
         <span className="block mb-1 font-bold text-pteal text-xs uppercase tracking-widest">
           {category}
         </span>
-        <h2 className="font-semibold text-pgray text-xl tracking-wide">
+        <h2 className="font-semibold text-pgray lg:text-lg 2xl:text-xl tracking-wide">
           {title}
         </h2>
       </div>
-      <p className="text-justify text-sgray text-sm">{description}</p>
+      <p className="text-justify text-sgray text-sm truncate">{description}</p>
     </Link>
   );
 }
